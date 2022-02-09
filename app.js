252776f8562f5fbe0d613b33e78391460b7fad6d
Jhon Ortiz <nickolas.ortiz.r@gmail.com>
2022-02-09 17:22:41 -0300

    termino

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-let pesos_1 = document.getElementById('pesos-1')
-let dolares_1 = document.getElementById('dolares-1')
-let boton = document.getElementById('boton')
+let pesos_1 = document.getElementById('dolares-1')
+let dolares_1 = document.getElementById('pesos-1')
+let boton_1 = document.getElementById('boton-1')
+let pesos_2 = document.getElementById('dolares-2')
+let dolares_2 = document.getElementById('pesos-2')
+let boton_2 = document.getElementById('boton-2')
 let dolar;
 let valor;
 let dolares;
@@ -20,4 +23,13 @@ let principal = async()=>{
     dolares_1.innerHTML = dolares;
 }
 
-boton.addEventListener('click',principal)
+boton_1.addEventListener('click',principal)
+
+let secundario = async()=>{
+    dolar = await respuesta();
+    valor = dolar.blue.value_sell;
+    dolares = valor * pesos_2.value;
+    dolares_2.innerHTML = dolares;
+}
+
+boton_2.addEventListener('click',secundario)
